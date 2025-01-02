24c68b570d1edbe897421458a083aafbc5e6a395
vish-6 <vishnuvardhan.j@thinkpalm.com>
2025-01-02 18:26:19 +0530

    now working as expected. Need some improvements

--- tutorials/Flags/flag.py
+++ tutorials/Flags/flag.py
@@ -2,7 +2,9 @@ import random
 
 from flask import Flask, render_template, request, redirect, url_for
 from random import randint
-from countries import africa, asia, europe, oceania, northAmerica, southAmerica, africa_dict, asia_dict, europe_dict, oceania_dict, southAmerica_dict, northAmerica_dict, asia_code, africa_code, europe_code, oceania_code, northAmerica_code, southAmerica_code
+from countries import africa, asia, europe, oceania, northAmerica, southAmerica, africa_dict, asia_dict, europe_dict, \
+    oceania_dict, southAmerica_dict, northAmerica_dict, asia_code, africa_code, europe_code, oceania_code, \
+    northAmerica_code, southAmerica_code
 
 app = Flask(__name__)
 continents = [
@@ -14,60 +16,76 @@ continents = [
     "oceania"
 ]
 
-
+global selected_continent
+global number_of_countries
+global question_no
+global random_values
+global counter
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# @app.route('/initialize', methods=['POST', 'GET'])
+
 def initialize():
+    global selected_continent
+    global number_of_countries
+    global question_no
+    global counter
+    selected_continent = None
+    number_of_countries = 0
+    question_no = 1
+    counter = 0
+
     continent = request.form.get('continent')
-    print(continent)
     if continent in globals():
-        temp = globals()[continent]
-        random.shuffle(temp)
-    print(temp)
-    number_of_countries = len(temp)
-    template_name = f"{continent}.html"
-    return temp, number_of_countries, template_name
-#    return render_template(template_name, asia_flag=temp[1].lower(), total=number_of_countries)
+        selected_continent = globals()[continent]
+        random.shuffle(selected_continent)
+    number_of_countries = len(selected_continent)
 
-def process_answer():
-    option1 = request.form.get("Option1")
-    option2 = request.form.get("Option2")
-    option3 = request.form.get("Option3")
-    print(option1, option2, option3)
-    return render_template('asia.html')
+    print("from initialize", selected_continent)
 
-@app.route('/asia', methods=['POST', 'GET'])
-def asia_route():
-    temp, number_of_countries, template_name = initialize()
-    return render_template(template_name, asia_flag=temp[1].lower(), total=number_of_countries)
+@app.route('/start', methods=['POST', 'GET'])
+def start():
+    initialize()
+    global random_values
+    random_values = random.sample(selected_continent, 2)
+    return render_template('play.html', flag_image=selected_continent[counter],
+                           total=number_of_countries,
+                           remaining=question_no, option1=selected_continent[counter],
+                           option2=random_values[0],
+                           option3=random_values[1], condition=False)
 
 
-@app.route('/continent/europe', methods=['POST', 'GET'])
-def europe():
-    return render_template('europe.html')
+@app.route('/play', methods=['POST', 'GET'])
+def play():
+    clicked = request.form.get("answer")
+    if clicked == selected_continent[0]:
+        return render_template('play.html', flag_image=selected_continent[counter],
+                               total=number_of_countries,
+                               remaining=question_no, option1=selected_continent[counter],
+                               option2=random_values[0],
+                               option3=random_values[1], condition=True)
+    else:
+        return render_template('play.html', flag_image=selected_continent[counter],
+                               total=number_of_countries,
+                               remaining=question_no, option1=selected_continent[counter],
+                               option2=random_values[0],
+                               option3=random_values[1], condition=True)
 
 
-@app.route('/continent/africa', methods=['POST', 'GET'])
-def africa():
-    return render_template('africa.html')
-
-
-@app.route('/continent/oceania', methods=['POST', 'GET'])
-def oceania():
-    return render_template('oceania.html')
-
-
-@app.route('/continent/southAmerica', methods=['POST', 'GET'])
-def southAmerica():
-    return render_template('southAmerica.html')
-
-
-@app.route('/continent/northAmerica', methods=['POST', 'GET'])
-def northAmerica():
-    return render_template('northAmerica.html')
+@app.route('/nxt', methods=['POST', 'GET'])
+def nxt():
+    global question_no, selected_continent, counter
+    question_no += 1
+    counter += 1
+    print("from nxt", selected_continent)
+    global random_values
+    random_values = random.sample(selected_continent, 2)
+    return render_template('play.html', flag_image=selected_continent[counter],
+                           total=number_of_countries,
+                           remaining=question_no, option1=selected_continent[counter],
+                           option2=random_values[0],
+                           option3=random_values[1], condition=False)
 
 
 if __name__ == "__main__":
